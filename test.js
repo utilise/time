81d9fdbf19c1c78a03038c6da5b927b52d4dd7a3
pemrouz <p3pedram@hotmail.com>
2016-05-07 19:25:58 +0100

    should work with undefined time

--- test.js
+++ test.js
@@ -10,4 +10,13 @@ describe('time', function() {
     time(75, function(){ expect(result).to.be.ok })
     time(99, done)
   })
+
+  it('should work with undefined time', function(done) {
+    var result = false
+      , fn = function(){ result = true }
+
+    time(fn)
+    time(5, function(){ expect(result).to.be.ok })
+    time(10, done)
+  })
 })
